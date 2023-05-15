7cc7a327943fb0f16d5c6a092677db82d8be4bed
Vale <oirelav95@gmail.com>
2023-05-15 17:24:14 +0200

    create component Scorri.vue

---
+++ src/components/Scorri.vue
@@ -0,0 +1,40 @@
+<script>
+
+export default {
+    name: 'Scorri',
+}
+</script>
+
+<template>
+    <section>
+        <h1>
+            Scorri
+        </h1>
+
+        <div class="list">
+            <div class="cards active">
+                <img src="" alt="SCORRI">
+
+                <h2>
+                    MODELLO A PANNELLI SCORREVOLI
+                </h2>
+
+                Nome modello: SCORRI
+
+                <p>
+                    Il modello SCORRI &egrave; una zanzariera ad ante scorrevoli o saliscendi (a due o pi&ugrave; ante).
+                    Fornito di serie con la rete in fibra di vetro, su richiesta pu&ograve; essere realizzato anche con rete
+                    in alluminio, acciaio, petscreen.
+                </p>
+
+
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../src/styles/general.scss' as *;
+@use '../src/styles/partials/mixins' as *;
+@use '../src/styles/partials/variables' as *;
+</style>
