bc4bf7d1da6c28d9a9e31c6dfdd296c52995a6ef
Vale <oirelav95@gmail.com>
2023-05-15 17:23:43 +0200

    create component Fissa.vue

---
+++ src/components/Fissa.vue
@@ -0,0 +1,39 @@
+<script>
+
+export default {
+    name: 'Fissa',
+}
+</script>
+
+<template>
+    <section>
+        <h1>
+            Fissa
+        </h1>
+
+        <div class="list">
+            <div class="cards active">
+                <img src="/img/porta-a-battente.jpg" alt="ANTAREX">
+
+                <h2>
+                    TELAIO FISSO
+                </h2>
+
+                Nome modello: FISSA
+
+                <p>
+                    Il modello FISSA &egrave; viene fornito di serie con rete in fibra di vetro, su richiesta pu&ograve;
+                    essere realizzato anche con rete in alluminio, acciaio, petscreen.
+                </p>
+
+
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../src/styles/general.scss' as *;
+@use '../src/styles/partials/mixins' as *;
+@use '../src/styles/partials/variables' as *;
+</style>
