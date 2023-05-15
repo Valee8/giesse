9477df9575d2bed7a6dd6d61d765808cc8906134
Vale <oirelav95@gmail.com>
2023-05-15 17:24:05 +0200

    create component PortaABattente.vue

---
+++ src/components/PortaABattente.vue
@@ -0,0 +1,41 @@
+<script>
+
+export default {
+    name: 'PortaABattente',
+}
+</script>
+
+<template>
+    <section>
+        <h1>
+            Porta a battente
+        </h1>
+
+        <div class="list">
+            <div class="cards active">
+                <img src="/img/porta-a-battente.jpg" alt="ANTAREX">
+
+                <h2>
+                    PORTA A BATTENTE
+                </h2>
+
+                Nome modello: ANTAREX
+
+                <p>
+                    Il modello ANTAREX &egrave; una zanzariera a battente per portafinestra. Caratterizzata dall'assenza di
+                    profili a pavimento, pu&ograve; essere realizzata ad una o pi&ugrave; ante. Fornita di serie con la rete
+                    in fibra di vetro, su richiesta pu&ograve; essere fornita anche con rete in alluminio, acciaio,
+                    petscreen.
+                </p>
+
+
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../src/styles/general.scss' as *;
+@use '../src/styles/partials/mixins' as *;
+@use '../src/styles/partials/variables' as *;
+</style>
