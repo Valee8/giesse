b7d42620ff41e201d1293175bb66db14ee4949c3
Vale <oirelav95@gmail.com>
2023-05-18 23:08:54 +0200

    edit component Fissa.vue

--- src/components/Fissa.vue
+++ src/components/Fissa.vue
@@ -7,26 +7,30 @@ export default {
 
 <template>
     <section>
-        <h1>
-            Fissa
-        </h1>
+        <div class="container">
+            <h1>
+                Fissa
+            </h1>
 
-        <div class="list">
-            <div class="cards active">
-                <img src="/img/porta-a-battente.jpg" alt="ANTAREX">
+            <div class="small_container">
+                <div class="cards active">
+                    <img src="/img/porta-a-battente.jpg" alt="ANTAREX">
 
-                <h2>
-                    TELAIO FISSO
-                </h2>
+                    <h2>
+                        TELAIO FISSO
+                    </h2>
 
-                Nome modello: FISSA
+                    <div class="name">
+                        FISSA
+                    </div>
 
-                <p>
-                    Il modello FISSA &egrave; viene fornito di serie con rete in fibra di vetro, su richiesta pu&ograve;
-                    essere realizzato anche con rete in alluminio, acciaio, petscreen.
-                </p>
+                    <p>
+                        Il modello FISSA &egrave; viene fornito di serie con rete in fibra di vetro, su richiesta pu&ograve;
+                        essere realizzato anche con rete in alluminio, acciaio, petscreen.
+                    </p>
 
 
+                </div>
             </div>
         </div>
     </section>
@@ -36,4 +40,8 @@ export default {
 @use '../src/styles/general.scss' as *;
 @use '../src/styles/partials/mixins' as *;
 @use '../src/styles/partials/variables' as *;
+
+.small_container {
+    text-align: center;
+}
 </style>
