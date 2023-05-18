ad9518b023c431ab64b3e7d8f21e52938746df46
Vale <oirelav95@gmail.com>
2023-05-18 23:09:34 +0200

    edit component PortaABattente.vue

--- src/components/PortaABattente.vue
+++ src/components/PortaABattente.vue
@@ -7,28 +7,33 @@ export default {
 
 <template>
     <section>
-        <h1>
-            Porta a battente
-        </h1>
+        <div class="container">
+            <h1>
+                Porta a battente
+            </h1>
 
-        <div class="list">
-            <div class="cards active">
-                <img src="/img/porta-a-battente.jpg" alt="ANTAREX">
+            <div class="small_container">
+                <div class="cards active">
+                    <img src="/img/porta-a-battente.jpg" alt="ANTAREX">
 
-                <h2>
-                    PORTA A BATTENTE
-                </h2>
+                    <h2>
+                        PORTA A BATTENTE
+                    </h2>
 
-                Nome modello: ANTAREX
+                    <div class="name">
+                        ANTAREX
+                    </div>
 
-                <p>
-                    Il modello ANTAREX &egrave; una zanzariera a battente per portafinestra. Caratterizzata dall'assenza di
-                    profili a pavimento, pu&ograve; essere realizzata ad una o pi&ugrave; ante. Fornita di serie con la rete
-                    in fibra di vetro, su richiesta pu&ograve; essere fornita anche con rete in alluminio, acciaio,
-                    petscreen.
-                </p>
+                    <p>
+                        Il modello ANTAREX &egrave; una zanzariera a battente per portafinestra. Caratterizzata dall'assenza
+                        di
+                        profili a pavimento, pu&ograve; essere realizzata ad una o pi&ugrave; ante. Fornita di serie con la
+                        rete
+                        in fibra di vetro, su richiesta pu&ograve; essere fornita anche con rete in alluminio, acciaio,
+                        petscreen.
+                    </p>
 
-
+                </div>
             </div>
         </div>
     </section>
@@ -38,4 +43,8 @@ export default {
 @use '../src/styles/general.scss' as *;
 @use '../src/styles/partials/mixins' as *;
 @use '../src/styles/partials/variables' as *;
+
+.small_container {
+    text-align: center;
+}
 </style>
