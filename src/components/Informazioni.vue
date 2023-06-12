04bb9737f22725e107e2ab92941a22715d810756
Vale <oirelav95@gmail.com>
2023-06-12 12:26:21 +0200

    create component Informazioni.vue

---
+++ src/components/Informazioni.vue
@@ -0,0 +1,27 @@
+<script>
+
+export default {
+    name: 'Informazioni',
+}
+</script>
+
+<template>
+    <section>
+        <h1>
+            Informazioni
+        </h1>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../src/styles/general.scss' as *;
+@use '../src/styles/partials/mixins' as *;
+@use '../src/styles/partials/variables' as *;
+
+section {
+    background-color: #000;
+    text-align: center;
+    color: #fff;
+    min-height: 400px;
+}
+</style>
