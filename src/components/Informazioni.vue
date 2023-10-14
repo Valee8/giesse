53e8e653c3d0320f6a44abe68894c1b2c61da6e5
Vale <oirelav95@gmail.com>
2023-10-14 20:23:32 +0200

    edit content Informazioni.vue

--- src/components/Informazioni.vue
+++ src/components/Informazioni.vue
@@ -1,6 +1,7 @@
 <script>
 
 import axios from 'axios';
+import { toHandlers } from 'vue';
 
 const API_URL = 'http://localhost:8000/api/v1/';
 
@@ -8,8 +9,12 @@ export default {
     name: 'Informazioni',
     data() {
         return {
+            error2: "",
+            sizeVariable: "",
+            error: "",
             messageSuccess: "",
-            file: null,
+            files: [],
+            enableButton: false,
             newInfo: {
                 typology: "",
                 name: "",
@@ -18,57 +23,153 @@ export default {
                 vat_number: "",
                 telephone_number: "",
                 city_of_residence: "",
-                attached_file: "",
+                attached_files: [],
                 message: "",
                 infoEmail: ""
-            }
+            },
+            format: [
+                ".png",
+                ".jpg",
+                ".jpeg",
+                ".docx",
+                ".pdf"
+            ]
         }
+    },
+    computed: {
+        prova() {
+
+        },
+        firstStepValid() {
+            if (this.newInfo.telephone_number.trim() !== "" && this.newInfo.city_of_residence.trim() !== "" && this.newInfo.message !== "") {
+                this.enableButton = true;
+            }
+
+            if (this.newInfo.typology === "Privato") {
+                if (this.newInfo.name.trim() !== "" && this.newInfo.surname.trim() !== "" && this.enableButton) {
+                    return true;
+                }
+            }
+            else if (this.newInfo.typology === "Azienda") {
+                if (this.newInfo.agency_name.trim() !== "" && this.newInfo.vat_number.trim() !== "" && this.enableButton) {
+                    return true;
+                }
+            }
+            return false;
+
+        },
     },
     methods: {
         onFileChange(e) {
-            this.file = e.target.files[0];
+            this.files = e.target.files;
+
+            this.error = "";
+            this.error2 = "";
+
+
+            for (let i = 0; i < this.files.length; i++) {
+
+                if (parseInt(this.files[i].size) > 15728640) {
+                    this.error = "Dimensioni per questo file troppo grandi";
+                }
+
+                this.newInfo.attached_files[i] = this.files[i].name.replace(/\s/g, '');
+            }
+
+            for (let i = 0; i < this.format.length; i++) {
+
+
+                for (let j = 0; j < this.files.length; j++) {
+                    if (!this.files[j].name.endsWith(this.format[i])) {
+                    }
+                }
+            }
         },
-        uploadFile() {
+        deleteAttach() {
 
-            if (this.file) {
-                const formData = new FormData();
-                formData.append('file', this.file);
+            this.$refs.fileInput.value = null;
 
-                axios.post(API_URL + 'uploadFile', formData, {
-                    headers: {
-                        'Content-Type': 'multipart/form-data',
-                    },
-                })
-                    .then((response) => {
-                        //console.log('File caricato con successo');
-                    })
-                    .catch((error) => {
-                        console.error('Errore nel caricamento del file:', error);
-                    });
-            }
-            else {
-                console.error('Nessun file selezionato');
-            }
+            this.error = "";
+            this.error2 = "";
+        },
+        sendEmail() {
 
-            this.newInfo.infoEmail = "oirelav95@gmail.com";
+            if (this.firstStepValid) {
 
-            this.newInfo.attached_file = this.file.name;
+                if (this.files.length > 0) {
+                    const formData = new FormData();
+                    //formData.append('files', this.files);
 
-            axios.post(API_URL + 'message', this.newInfo)
-                .then(res => {
-                    const data = res.data;
-                    const success = data.success;
-
-                    if (success) {
-                        this.messageSuccess = "Messaggio inviato con successo!";
+                    for (let i = 0; i < this.files.length; i++) {
+                        formData.append('files', this.files[i]);
                     }
 
-                })
-                .catch(error => console.log(error));
+                    axios.post(API_URL + 'uploadFile', formData, {
+                        headers: {
+                            'Content-Type': 'multipart/form-data',
+                        },
+                    })
+                        .then((response) => {
+                            //console.log('File caricato con successo');
+                        })
+                        .catch((error) => {
+                            console.error('Errore nel caricamento del file:', error);
+                        });
+                }
+
+                // for (let i = 0; i < this.files.length; i++) {
+                //     this.sizes[i] = this.files[i].size;
+                // }
+
+                this.newInfo.infoEmail = "oirelav95@gmail.com";
+
+                axios.post(API_URL + 'message', this.newInfo)
+                    .then(res => {
+                        const data = res.data;
+                        const success = data.success;
+
+                        if (success) {
+                            this.messageSuccess = "Messaggio inviato con successo!";
+
+                            setTimeout(() => {
+                                location.reload();
+                            }, 2000);
+                        }
+
+                    })
+                    .catch(error => console.log(error.response.data));
+            }
         },
         handleSubmit(event) {
             event.preventDefault();
         },
+        filterNumbers() {
+            this.newInfo.name = this.newInfo.name.replace(/[0-9]/g, '');
+            this.newInfo.surname = this.newInfo.surname.replace(/[0-9]/g, '');
+        },
+        filterCharacters() {
+            this.newInfo.telephone_number = this.newInfo.telephone_number.replace(/\D/g, '');
+            this.newInfo.vat_number = this.newInfo.vat_number.replace(/\D/g, '');
+        },
+        handleSubmit(event) {
+            event.preventDefault();
+        },
+        resetCommonInputs() {
+            if (this.newInfo.typology) {
+                this.newInfo.name = "";
+                this.newInfo.surname = "";
+                this.newInfo.agency_name = "";
+                this.newInfo.vat_number = "";
+                this.newInfo.email = "";
+                this.newInfo.telephone_number = "";
+                this.newInfo.city_of_residence = "";
+            }
+        },
+    },
+    mounted() {
+        if (this.newInfo.typology === "") {
+            this.newInfo.typology = "Privato";
+        }
     }
 }
 </script>
@@ -106,32 +207,35 @@ export default {
                         <div class="inputs-top">
                             <div v-if="newInfo.typology === 'Privato' || newInfo.typology === ''">
                                 <input type="text" class="first-input" v-model="newInfo.name" placeholder="Nome *"
-                                    @input="filterNumbers" required>
+                                    @input="filterNumbers" title="Inserisci il nome" required>
                                 <input type="text" class="second-input" v-model="newInfo.surname" placeholder="Cognome *"
-                                    @input="filterNumbers" required>
+                                    @input="filterNumbers" title="Inserisci il cognome" required>
                             </div>
                             <div v-else-if="newInfo.typology === 'Azienda'">
                                 <input type="text" class="first-input" v-model="newInfo.agency_name"
-                                    placeholder="Nome Azienda *" required>
+                                    placeholder="Nome Azienda *" title="Inserisci il nome dell'azienda" required>
                                 <input type="text" class="second-input" v-model="newInfo.vat_number"
-                                    placeholder="Partita Iva *" maxlength="11" @input="filterCharacters" required>
+                                    placeholder="Partita Iva *" maxlength="11" @input="filterCharacters"
+                                    title="Inserisci la Partita Iva" required>
                             </div>
                         </div>
 
                         <div class="inputs-bottom">
                             <input type="text" class="first-input" v-model="newInfo.telephone_number"
-                                placeholder="Telefono *" @input="filterCharacters" required>
+                                placeholder="Telefono *" @input="filterCharacters" title="Inserisci il numero di telefono"
+                                required>
                             <input type="text" class="second-input" v-model="newInfo.city_of_residence"
-                                placeholder="Comune *" @input="filterNumbers" required>
+                                placeholder="Comune *" @input="filterNumbers" title="Inserisci il Comune" required>
                         </div>
 
-                        <textarea name="message" v-model="newInfo.message" rows="8" placeholder="Messaggio *"
-                            required></textarea>
+                        <textarea name="message" v-model="newInfo.message" rows="8" placeholder="Messaggio *" required
+                            title="Inserisci il messaggio"></textarea>
 
                         <div class="obligatory">* Campi obbligatori</div>
 
                         <div class="submit">
-                            <button type="submit" @click="uploadFile">Invia <i class="fa-regular fa-envelope"></i></button>
+                            <button type="submit" @click="sendEmail" :disabled="error !== '' || error2 !== ''">Invia
+                                <i class="fa-regular fa-envelope"></i></button>
                         </div>
                     </div>
 
@@ -148,11 +252,22 @@ export default {
 
                             <p>
                                 E' possibile allegare un file con i seguenti formati .png, .jpg, .jpeg, .docx, .pdf
-                                dimensione
-                                massima 20MB
+                                dimensione massima 15MB
                             </p>
 
-                            <input type="file" @change="onFileChange" ref="fileInput" id="file">
+                            <input type="file" @change="onFileChange" ref="fileInput" id="file"
+                                accept=".png, .jpg, .jpeg, .docx, .pdf" multiple>
+
+                            <div @click="deleteAttach" v-if="files.length > 0">Rimuovi file
+                                allegato</div>
+
+                            <div>
+                                {{ error }}
+                            </div>
+
+                            <div>
+                                {{ error2 }}
+                            </div>
                         </div>
 
                     </div>
