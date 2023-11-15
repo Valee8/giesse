3006d0a647d6ac258febfdc58d8f28f4c7a3198f
Vale <oirelav95@gmail.com>
2023-11-15 23:24:40 +0100

    edit image

--- src/components/PortaABattente.vue
+++ src/components/PortaABattente.vue
@@ -50,7 +50,7 @@ export default {
                 <div class="slider active">
                     <div class="div-image">
                         <!-- Immagine zanzariera -->
-                        <img src="/img/zanzariere/porta-a-battente.jpg" alt="ANTAREX" class="slide-image">
+                        <img src="/img/zanzariere/porta-a-battente.png" alt="ANTAREX" class="slide-image">
                     </div>
 
                     <!-- Titolo - nome modello -->
