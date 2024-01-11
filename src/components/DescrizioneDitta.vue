b64af61722003b9aea3d7e73c5a04882a313ef50
Vale <oirelav95@gmail.com>
2024-01-11 19:09:45 +0100

    edit description in DescrizioneDitta

--- src/components/DescrizioneDitta.vue
+++ src/components/DescrizioneDitta.vue
@@ -16,26 +16,40 @@ export default {
 
             <!-- Testo - descrizione ditta -->
             <p>
-                La <span>Giesse Zanzariere</span>, da pi&ugrave; di 30 anni impegnata nella realizzazione di zanzariere, è
+                La <span>Giesse Zanzariere</span>, da pi&ugrave; di 30 anni impegnata nella realizzazione di zanzariere,
+                &egrave;
                 attrezzata per la produzione di <span>ZANZARIERE SU MISURA</span> con rete saldata e bottoncini antivento.
-                Possono essere fornite con: <span>CUFFIE LATERALI</span> - <span>TESTATA SINGOLA</span> - <span>CASSONETTO
-                    DA 40mm</span>.
+                Le nostre zanzariere vengono prodotte <span>di serie</span> con il cassonetto da 50mm e possono essere
+                fornite con:
+                <span class="prova">
+                    <span>
+                        CUFFIE LATERALI
+                    </span>
+                    -
+                    <span>
+                        TESTATA SINGOLA
+                    </span>
+                    -
+                    <span>CASSONETTO DA 40mm
+                    </span>
+                </span>
                 La versione con <span>CUFFIE LATERALI E GUIDA TELESCOPICA</span> garantisce una facile installazione anche
                 nei vani con
                 notevoli fuori squadra, la versione con <span>TESTATA SINGOLA</span> invece &egrave; adatta soprattutto per
                 monoblocchi o
                 per vani regolari, infine la versione con <span>CASSONETTO DA 40mm</span> &egrave; adatta per spazi ridotti.
+            </p>
 
-                <!-- !!!!!! Forse da rimuovere !!!!!! -->
-            <div style="display: none">
+
+            <!-- !!!!!! Forse da rimuovere !!!!!! -->
+            <!-- <div style="display: none">
                 Con misura da <span>scontare</span> la zanzariera verr&agrave; fornita con 2mm in meno in LARGHEZZA e
                 ALTEZZA, con
                 misura <span>finita</span> la zanzariera verr&agrave; fornita con le stesse misure in LARGHEZZA e ALTEZZA,
                 invece con misura
                 <span>luce</span> verr&agrave; aumentata in LARGHEZZA e ALTEZZA in base alle varie tipologie dei modelli a
                 incasso.
-            </div>
-            </p>
+            </div> -->
         </div>
     </section>
 </template>
@@ -62,12 +76,18 @@ section {
 
         // Testo
         p {
-            text-align: center;
+            text-align: justify;
             margin: 0 auto;
-            max-width: 690px;
+            max-width: 725px;
             font-size: 1.1rem;
             font-weight: 600;
             padding: 30px 0;
+
+            .prova {
+                display: block;
+                margin: 7px 0;
+                text-align: center;
+            }
         }
     }
 }
