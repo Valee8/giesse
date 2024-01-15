6d0c441d25cb31b881e440f800b567bdb50f7215
Vale <oirelav95@gmail.com>
2024-01-15 13:31:30 +0100

    create file service-worker.js

--- src/main.js
+++ src/main.js
@@ -7,4 +7,18 @@ import { router } from './router';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 
+const serviceWorkerPath = '/service-worker.js';
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register(serviceWorkerPath)
+        .then(registration => {
+            console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch(error => {
+            console.error('Service Worker registration failed:', error);
+        });
+}
+
+
+
 createApp(App).use(router).mount('#app')
