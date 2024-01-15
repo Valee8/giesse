eedc031148575a23624821c42f5314c295c40592
Vale <oirelav95@gmail.com>
2024-01-15 16:09:39 +0100

    add comments

--- src/main.js
+++ src/main.js
@@ -7,19 +7,16 @@ import { router } from './router';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 
+// const serviceWorkerPath = process.env.NODE_ENV === 'production' ? '/giesse/service-worker.js' : '/service-worker.js';
 
-const serviceWorkerPath = process.env.NODE_ENV === 'production' ? '/giesse/service-worker.js' : '/service-worker.js';
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register(serviceWorkerPath)
-        .then(registration => {
-            console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch(error => {
-            console.error('Service Worker registration failed:', error);
-        });
-}
-
-
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register(serviceWorkerPath)
+//         .then(registration => {
+//             console.log('Service Worker registered with scope:', registration.scope);
+//         })
+//         .catch(error => {
+//             console.error('Service Worker registration failed:', error);
+//         });
+// }
 
 createApp(App).use(router).mount('#app')
